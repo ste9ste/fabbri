8b6fc0b40eba558d00d9dd8f5968b53a864ab68e
ste9ste <168522619+ste9ste@users.noreply.github.com>
2024-11-10 18:18:42 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Fabrix v0.1 (Novembre 2024)
+# Simulatore di processi produttivi nel settore secondario
+# Sviluppatore: Stefano Solazzi Contessa
+# File: main.py - Avviamento del programma principale
+
+# Importazione del modulo relativo all'interfaccia grafica
+from gui import gui
+
+# Punto di ingresso del software
+def main():
+    gui() # Avviamento interfaccia grafica
+
+# Esecuzione della funzione principale
+if __name__ == "__main__": main()
